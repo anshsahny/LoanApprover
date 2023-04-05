a67b4b10bc7c76c95772f353c24409f4ba5a447c
Ansh Sahny <anshsahny@gmail.com>
2023-04-04 21:25:17 -0400

    Initial commit for frontend of project

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import logging
+import joblib
+import json
+import sys
+import os
+
+current_dir = os.path.dirname(__file__)
+
+app = Flask(__name__, static_folder='static', template_folder='template')
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
