e822786e891c111b9702602ba4c9821f2917da39
Ansh Sahny <anshsahny@gmail.com>
2023-04-15 22:20:13 -0400

    Added predict function in backend

--- app.py
+++ app.py
@@ -15,20 +15,33 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 
+# Function that handles the ML prediction
+def prediction(data=pd.DataFrame):
+    # Get model and file path
+    model_name = 'bin/xgboostModel.pkl'
+    model_dir = os.path.join(current_dir, model_name)
+    # Load the model
+    loaded_model = joblib.load(open(model_dir, 'rb'))
+    # Predict the result
+    result = loaded_model.predict(data)
+    # Return the result
+    return result[0]
+
+
 # Home Page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Result Page
-@app.route('/prediction')
-def prediction():
-    return render_template('prediction.html')
 
-# Error Page
-@app.route('/error')
-def error():
-    return render_template('error.html')
+# Prediction Page
+@app.route('/prediction', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        return render_template('prediction.html')
+    else:
+        # Return Error Page
+        return render_template('error.html')
 
 
 if __name__ == '__main__':
