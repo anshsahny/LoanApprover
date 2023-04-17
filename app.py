b97849039b48e8a57fcc987d9982f5b068b77406
Ansh Sahny <anshsahny@gmail.com>
2023-04-16 20:58:38 -0400

    Initial commit for prediction POST call

--- app.py
+++ app.py
@@ -38,7 +38,27 @@ def home():
 @app.route('/prediction', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        return render_template('prediction.html')
+        # Save form data
+        name = request.form['name']
+        gender = request.form['gender']
+        education = request.form['education']
+        self_employed = request.form['self_employed']
+        marital_status = request.form['marital_status']
+        applicant_income = request.form['applicant_income']
+        coapplicant_income = request.form['coapplicant_income']
+        loan_amount = request.form['loan_amount']
+        loan_term = request.form['loan_term']
+        credit_history = request.form['credit_history']
+        property_area = request.form['property_area']
+
+        # Load JSON file with column names
+        schema_name = 'data/columns_set.json'
+        schema_dir = os.path.join(current_dir, schema_name)
+        with open(schema_dir, 'r') as f:
+            cols = json.loads(f.read())
+        schema_cols = cols['data_columns']
+
+        # return render_template('prediction.html')
     else:
         # Return Error Page
         return render_template('error.html')
