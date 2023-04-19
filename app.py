457676a7919fa6b737e6b67b1afbdcd6ee7b4943
Ansh Sahny <anshsahny@gmail.com>
2023-04-18 21:02:55 -0400

    Completion of application

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.logger.setLevel(logging.ERROR)
 
 
 # Function that handles the ML prediction
-def prediction(data=pd.DataFrame):
+def predictor(data=pd.DataFrame):
     # Get model and file path
     model_name = 'bin/xgboostModel.pkl'
     model_dir = os.path.join(current_dir, model_name)
@@ -44,6 +44,7 @@ def predict():
         education = request.form['education']
         self_employed = request.form['self_employed']
         marital_status = request.form['marital_status']
+        dependents = request.form['dependents']
         applicant_income = request.form['applicant_income']
         coapplicant_income = request.form['coapplicant_income']
         loan_amount = request.form['loan_amount']
@@ -58,7 +59,52 @@ def predict():
             cols = json.loads(f.read())
         schema_cols = cols['data_columns']
 
-        # return render_template('prediction.html')
+        # Parse categorical columns
+        try:
+            col = ('Dependents_' + str(dependents))
+            if col in schema_cols.keys():
+                schema_cols[col] = 1
+            else:
+                pass
+        except:
+            pass
+
+        try:
+            col = ('Property_Area_' + str(property_area))
+            if col in schema_cols.keys():
+                schema_cols[col] = 1
+            else:
+                pass
+        except:
+            pass
+
+        # Parse numerical columns
+        schema_cols['ApplicantIncome'] = applicant_income
+        schema_cols['CoapplicantIncome'] = coapplicant_income
+        schema_cols['LoanAmount'] = loan_amount
+        schema_cols['Loan_Amount_Term'] = loan_term
+        schema_cols['Gender_Male'] = gender
+        schema_cols['Married_Yes'] = marital_status
+        schema_cols['Education_Not Graduate'] = education
+        schema_cols['Self_Employed_Yes'] = self_employed
+        schema_cols['Credit_History_1.0'] = credit_history
+
+        # Convert JSON into data frame
+        df = pd.DataFrame(
+            data={k: [v] for k, v in schema_cols.items()},
+            dtype=float
+        )
+
+        # Predict from form results
+        result = predictor(data=df)
+
+        # Determine output
+        if int(result) == 1:
+            prediction = 'Dear Mr/Mrs/Ms {name}, your loan is approved!'.format(name=name)
+        else:
+            prediction = 'Sorry Mr/Mrs/Ms {name}, your loan is rejected!'.format(name=name)
+
+        return render_template('prediction.html', prediction=prediction)
     else:
         # Return Error Page
         return render_template('error.html')
